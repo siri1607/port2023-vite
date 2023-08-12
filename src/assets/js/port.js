310ff1d417ad85f06e1269c485d93741548f7237
miseon <siri160710@gmail.com>
2023-08-12 13:25:44 +0900

    포트폴리오 완료

--- src/assets/js/port.js
+++ src/assets/js/port.js
@@ -1,3 +1,22 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
 export function port(){
-    console.log(port);
+    gsap.registerPlugin(ScrollTrigger);
+    const horScreen = gsap.utils.toArray(".port_item");
+
+    gsap.to(horScreen, {
+        xPercent: -120 * (horScreen.length - 1),
+        ease: "none",
+        scrollTrigger: {
+            trigger: "#port",
+            start: "top 56px",
+            end: "+=3000",
+            pin: true,
+            scrub: 1,
+            markers: true,
+            invalidateOnRefresh: true,
+            aniticipatePin: 1,
+        },
+    });
 }
